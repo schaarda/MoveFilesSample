5824be4e818727fd281dd68098429ed367aa26d6
schaarda <Schaarschmidt_Daniel@web.de>
2023-05-26 14:36:39 +0200

    Umbau zu Transformer

--- modules/ComponentB/build.gradle.kts
+++ modules/ComponentB/build.gradle.kts
@@ -2,17 +2,80 @@ plugins {
     `java`
 }
 group = "org.example"
-dependencies {
-    testImplementation(testFixtures("org.example:ComponentA"))
+
+// eigenes Attribut definieren
+val etcExtracted = Attribute.of("etcExtracted", Boolean::class.javaObjectType)
+// eigene Configuration damit es nur die gewünschte Dep trifft
+val etcConfiguration by configurations.creating {
+    isTransitive = false
+    // etcExtracted=true wird benötigt
+    attributes.attribute(etcExtracted,true)
 }
+
+configurations {
+    testImplementation {
+        extendsFrom(etcConfiguration)   // so muss ich die Dep die auch in etcconfig ist nicht 2* definieren
+        isTransitive = true
+    }
+}
+
+configurations.all {
+    afterEvaluate {
+        if (isCanBeResolved) {
+            resolve()        // trigger resolve to test
+        }
+    }
+}
+
+// Transformer zum Entpacken
+abstract class Unzip : TransformAction<TransformParameters.None> {
+    @get:InputArtifact
+    abstract val inputArtifact: Provider<FileSystemLocation>
+
+    override
+    fun transform(outputs: TransformOutputs) {
+        val input = inputArtifact.get().asFile
+        val unzipDir = outputs.dir(input.name)
+        unzipTo(input, unzipDir)
+    }
+
+    private fun unzipTo(zipFile: File, unzipDir: File) {
+        println("unzip $zipFile to $unzipDir")
+        println("File exists? ${zipFile.exists()}")
+    }
+}
+
+
+
+dependencies {
+    // eigenes Attribut registrieren
+    attributesSchema {
+        attribute(etcExtracted)
+    }
+    // Transformer registrieren
+    registerTransform(Unzip::class) {
+        from.attribute(etcExtracted,false)
+        to.attribute(etcExtracted,true)
+    }
+
+    // und jede jar hat das Attribut per default auf false
+    artifactTypes.getByName("jar") {
+        attributes.attribute(etcExtracted, false)
+    }
+    // zu entpackende Dep
+    etcConfiguration(testFixtures("org.example:ComponentA"))
+}
+
+
 
 val a by tasks.registering(Task::class) {
     dependsOn("clean","assemble")
     doLast{
-        var x = configurations["testCompileClasspath"].copyRecursive{it.group == "org.example" && it.name=="ComponentA"}
-        resources.text.fromArchiveEntry(x,"tomcat.p12")
-            .asFile().copyTo(file("build/testFolder/tomcat.p12"))
+        println("hi")
     }
+
+
+
 
 
 }
